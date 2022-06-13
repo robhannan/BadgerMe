0ce4e5444b5d7d69a79bf24a10f43e50f02dfb45
Rob Hannan <rob.hannan@red-badger.com>
2022-06-13 17:17:26 +0100

    Add interest setup page

---
+++ app/src/main/java/com/redbadger/badgerme/InterestSetup.kt
@@ -0,0 +1,11 @@
+package com.redbadger.badgerme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterestSetup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interest_setup)
+    }
+}
