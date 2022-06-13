e17fda29b12049800f16d72850135442c69835df
Rob Hannan <rob.hannan@red-badger.com>
2022-06-13 12:37:56 +0100

    Get google login working

--- app/src/main/java/com/redbadger/badgerme/GoogleLogin.kt
+++ app/src/main/java/com/redbadger/badgerme/GoogleLogin.kt
@@ -1,10 +1,13 @@
 package com.redbadger.badgerme
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.gms.auth.api.signin.*
 
 
 class GoogleLogin : AppCompatActivity() {
@@ -13,12 +16,17 @@ class GoogleLogin : AppCompatActivity() {
         setContentView(R.layout.activity_google_login)
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
 
-        // Build a GoogleSignInClient with the options specified by gso.
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        // Build a GoogleSignInClient with the options specified by googleSignInOptions.
+        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        findViewById<ConstraintLayout>(R.id.sign_in_button).setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            signInActivityResultLauncher.launch(signInIntent)
+        }
     }
 
     override fun onStart() {
@@ -29,7 +37,18 @@ class GoogleLogin : AppCompatActivity() {
         updateUI(account)
     }
 
-    fun updateUI(account: GoogleSignInAccount?) {
+    private val signInActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                Toast.makeText(this@GoogleLogin, "Accepted", Toast.LENGTH_SHORT).show()
+            }
+        }
 
+    private fun updateUI(account: GoogleSignInAccount?) {
+        account?.let {
+            findViewById<ConstraintLayout>(R.id.sign_in_button).apply{
+                this.visibility = View.INVISIBLE
+            }
+        }
     }
 }
