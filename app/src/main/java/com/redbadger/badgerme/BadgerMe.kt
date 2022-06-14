259d15d63e90941bb4fd68065bd2e70bf22cf4ac
Rob Hannan <rob.hannan@red-badger.com>
2022-06-14 15:51:50 +0100

    Change to single-activity app

--- app/src/main/java/com/redbadger/badgerme/BadgerMe.kt
+++ app/src/main/java/com/redbadger/badgerme/BadgerMe.kt
@@ -1,11 +1,12 @@
 package com.redbadger.badgerme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class InterestSetup : AppCompatActivity() {
+
+class BadgerMe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interest_setup)
+        setContentView(R.layout.activity_badger_me)
     }
 }
